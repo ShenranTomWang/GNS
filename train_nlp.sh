b3860125f28212de84640cdbafb55c8214bea7a2
ShenranTomWang <wsr.20021128@gmail.com>
2025-11-17 16:00:22 -0800

    debug

--- train_nlp.sh
+++ train_nlp.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --ntasks=4
-#SBATCH --mem=24G
-#SBATCH --time=2-00:00:00
-#SBATCH --account=st-hgonen-1-gpu
-#SBATCH --output=train.log
-#SBATCH --error=train.log
-#SBATCH --mail-user=shenranw@student.ubc.ca
-#SBATCH --mail-type=ALL
-#SBATCH --job-name=train
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=12:0:0
+#SBATCH --partition=nlpgpo
 
 source /home/shenranw/.bashrc
 cd /ubc/cs/home/s/shenranw/GNS
 source ../scratch/envs/GNS/.venv/bin/activate
 
-bash ./download_dataset.sh WaterDropSample /ubc/cs/home/s/shenranw/scratch/datasets/
+# bash ./download_dataset.sh WaterDropSample /ubc/cs/home/s/shenranw/scratch/datasets/
 
 python train_or_infer.py \
     --data_dir /ubc/cs/home/s/shenranw/scratch/datasets/WaterDropSample \
